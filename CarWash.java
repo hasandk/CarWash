cdf3dcb92e14d6d003733c9503371d44ce3a2103
heidaridk <36068229+heidaridk@users.noreply.github.com>
2018-04-09 10:25:00 +0200

    den nye

--- CarWash.java
+++ CarWash.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 
 public class CarWash {
- //lave en arraylist, 
+ //lave en arraylist,  
     ArrayList<Customer> cl;
     ArrayList<WashCard> wcl;
     Customer c1, c2, c3;
@@ -12,20 +12,20 @@ public class CarWash {
     WashCard wc2 = null;
     WashCard wc3 = null;
     Scanner scanner = new Scanner(System.in);
- 
+   //trin 1
     private void register() {
         cl = new ArrayList<>();
         wcl = new ArrayList<>();
-
+        //lav customers
         c1 = new Customer(1, "Hasan", "Haidari", "4444444");
-        c2 = new Customer(2, "Reza", "Ha", "354656");
+        c2 = new Customer(2, "Daniel", "Ha", "354656");
         c3 = new Customer(3, "Mama", "ghahwei", "8880987");
         cl.add(c1);
         cl.add(c2);
         cl.add(c3);
-
-        wc1 = new WashCard(1, "Hasan", 2000);
-        wc2 = new WashCard(2, "Reza", 2000);
+        //lav Cards
+        wc1 = new WashCard(1, "Hasan", 500);
+        wc2 = new WashCard(2, "Daniel", 2000);
         wc3 = new WashCard(3, "Mama", 2000);
         wcl.add(wc1);
         wcl.add(wc2);
@@ -33,7 +33,9 @@ public class CarWash {
 
         System.out.print("\n Customers are registered and wash card is published.\n");
     }
-// lave forloop
+    
+    //trin 2
+    // lave forloops for at kunne finde rette personer, id'er
     private void login(int cpr) {
         boolean result = false;
         int i = 0; // Counter used to determine customers
@@ -44,6 +46,7 @@ public class CarWash {
                     System.out.print("\n *************Hello " + c.getName() + "**************\n");
                     System.out.print("1. add balance 2. wash your car 3. Logout\t "
                             + "\n Enter a number: \t");
+                     //trin 3
                     int nummer = scanner.nextInt();
                     if (nummer == 1) {
                         System.out.print("\n Insert cash: \t");
@@ -62,9 +65,9 @@ public class CarWash {
                         int x = 0;
                         for (WashCard wc : wcl) {
                             if (wc.cardId == c.getCpr()) {
-                                if (wc.getBalance() > 200) {
+                                if (wc.getBalance() > 200) { //prisen på vasken
                                     int amount = 200;
-                                    wc.minusBalance(amount);
+                                    wc.minusBalance(amount); //træker den fra
                                     System.out.print("\n Washing cost is: 200 DKK\n");
                                     System.out.print("\n your balance is " + wc.getBalance() + " now.\n");
                                 } else {
@@ -102,7 +105,7 @@ public class CarWash {
             System.out.print("\n1. Register 2. Login 3. Exit"
                     + "\n Enter a number:\t");
 
-            int number = sc.nextInt();
+            int number = sc.nextInt(); //istedet for if else statement
             switch (number) {
                 case 1:
                     cw.register();
